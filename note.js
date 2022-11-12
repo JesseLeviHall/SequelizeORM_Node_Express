fcfa8073a2a5ebd489a9fa3e894081cab71842c5
jesse h <jesselevihall@gmail.com>
2022-11-12 09:27:06 -0700

    class based model

--- note.js
+++ note.js
@@ -4,6 +4,6 @@ ORM stands for Object Relational Mapping.
 ORM is a technique that lets you query and manipulate data from a relational database using an object-oriented paradigm.
 Sequelize is a promise-based ORM for Node.js v4 and up. It supports the dialects PostgreSQL, MySQL, SQLite and MSSQL and features solid transaction support, relations, read replication and more.
 
-there are two ways to construct a model. 
+there are two ways to construct a model. Class based or function based. Class worked better for me. 
 
 */
