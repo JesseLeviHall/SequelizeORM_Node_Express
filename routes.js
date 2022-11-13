5e15c2bf3a8676641be6a78144c9067d62e0892c
jesse h <jesselevihall@gmail.com>
2022-11-13 15:39:18 -0700

    plain sql query

--- routes.js
+++ routes.js
@@ -104,4 +104,20 @@ router.delete('/users/:id', async (req, res) => {
 	}
 });
 
+//raw sql query:
+router.get('/raw', async (req, res) => {
+	try {
+		const users = await sequelize.query(
+			'SELECT * FROM users', //anysql goes here
+			{
+				type: sequelize.QueryTypes.SELECT,
+			}
+		);
+		res.status(200).json(users);
+	} catch (err) {
+		res.status(500).json(err);
+		console.log(err);
+	}
+});
+
 module.exports = router;
